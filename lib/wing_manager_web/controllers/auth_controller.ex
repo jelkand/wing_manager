ef207dd2b68f1b034ab7ca24837083240471f47a
Jack Anderson <jack@jackanderson.dev>
2023-02-01 23:01:20 -0700

    basic auth via discord

--- lib/wing_manager_web/controllers/auth_controller.ex
+++ lib/wing_manager_web/controllers/auth_controller.ex
@@ -1,22 +1,37 @@
 defmodule WingManagerWeb.AuthController do
   use WingManagerWeb, :controller
-  plug Ueberauth
+  plug(Ueberauth)
+
+  alias WingManager.{Accounts, Accounts.Guardian}
 
   def request(_conn, _params) do
     # Present an authentication challenge to the user
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth_data}} = conn, _params) do
-    # IO.inspect(auth_data)
-    # Find the account if it exists or create it if it doesn't
+    case Accounts.get_or_register_by_discord_id(auth_data) do
+      {:ok, account} ->
+        conn
+        |> Guardian.Plug.sign_in(account)
+        |> redirect(to: ~p"/")
 
-    conn
+      {:error, _error_changeset} ->
+        conn
+        |> put_flash(:error, "Authentication failed.")
+        |> redirect(to: ~p"/  ")
+    end
   end
 
   def callback(%{assigns: %{ueberauth_failure: _}} = conn, _params) do
     # Tell the user something went wrong
     conn
     |> put_flash(:error, "Authentication failed.")
-    |> redirect(to: ~p"/register")
+    |> redirect(to: ~p"/")
+  end
+
+  def logout(conn, _) do
+    conn
+    |> Guardian.Plug.sign_out()
+    |> redirect(to: ~p"/")
   end
 end
