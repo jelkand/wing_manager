d10d5ee943cf9f41507494d416c25b70eabe485b
Jack Anderson <jack@jackanderson.dev>
2023-02-07 21:02:47 -0500

    fix tenant live

--- lib/wing_manager_web/live_tenant.ex
+++ lib/wing_manager_web/live_tenant.ex
@@ -1,27 +1,31 @@
 defmodule WingManagerWeb.LiveTenant do
   use WingManagerWeb, :verified_routes
-  import Phoenix.Component
+  # import Phoenix.Component
   alias WingManager.Wing
   @tenant_param "wing"
 
-  defp mount_current_tenant(session, socket) do
-    case session do
+  defp mount_current_tenant(params, socket) do
+    IO.inspect({@tenant_param, params}, label: "Mounting tenant")
+
+    case params do
       %{@tenant_param => tenant_slug} ->
         Phoenix.Component.assign_new(socket, :current_tenant, fn ->
           Wing.get_tenant_by_slug(tenant_slug)
         end)
 
       %{} ->
+        IO.puts("didn't get tenant")
         Phoenix.Component.assign_new(socket, :current_tenant, fn -> nil end)
     end
   end
 
-  def on_mount(:mount_current_tenant, _params, session, socket) do
-    {:cont, mount_current_tenant(session, socket)}
+  def on_mount(:mount_current_tenant, params, _session, socket) do
+    IO.inspect(params, label: "mounting tenant")
+    {:cont, mount_current_tenant(params, socket)}
   end
 
-  def on_mount(:ensure_tenant, _params, session, socket) do
-    socket = mount_current_tenant(session, socket)
+  def on_mount(:ensure_tenant, params, _session, socket) do
+    socket = mount_current_tenant(params, socket)
 
     if socket.assigns.current_tenant do
       {:cont, socket}
@@ -35,8 +39,8 @@ defmodule WingManagerWeb.LiveTenant do
     end
   end
 
-  def on_mount(:redirect_if_has_tenant, _params, session, socket) do
-    socket = mount_current_tenant(session, socket)
+  def on_mount(:redirect_if_has_tenant, params, _session, socket) do
+    socket = mount_current_tenant(params, socket)
 
     if socket.assigns.current_tenant do
       {:halt, Phoenix.LiveView.redirect(socket, to: tenant_home_path(socket))}
